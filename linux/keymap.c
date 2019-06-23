58bee27de552bc96be935ab59afe2a839a87b42f
Luke deGruchy <ldegruchy@gmail.com>
2019-06-23 18:28:13 -0400

    Many changes.

--- linux/keymap.c
+++ linux/keymap.c
@@ -39,60 +39,60 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Base Layer
  *
  * ,-----------------------------------------------------.           ,-----------------------------------------------------.
- * | SHIFT+INS | 1  ! | 2  @ | 3  # | 4  $ | 5  % |GUI+LT|           |GUI+RT| 6  ^ | 7  & | 8  * | 9  ( | 10 ) |   DEL     |
+ * |     ~     | 1  ! | 2  @ | 3  # | 4  $ | 5  % |GUI+LT|           |GUI+RT| 6  ^ | 7  & | 8  * | 9  ( | 10 ) |   DEL     |
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
- * |     ~     |   Q  |   W  |   E  |   R  |   T  |OneSht|           |OneSht|   Y  |   U  |   I  |   O  |   P  |     -     |
+ * |    TAB    |   Q  |   W  |   E  |   R  |   T  |OneSht|           |OneSht|   Y  |   U  |   I  |   O  |   P  |     -     |
  * |-----------+------+------+------+------+------|Mouse |           |Symbol|------+------+------+------+------+-----------|
  * |     =     |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |     '     |
- * |-----------+------+------+------+------+------| TAB  |           | APPS |------+------+------+------+------+-----------|
+ * |-----------+------+------+------+------+------|SHIFT |           | SHIFT|------+------+------+------+------+-----------|
  * |   SHIFT   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   SHIFT   |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
  *     |  GUI  |CT+ALT|SF+CTL| Left | Right|                                       | Down |  Up  |SF+CTL|CT+ALT|  GUI  |
  *     `-----------------------------------'                                       `-----------------------------------'
  *                                         ,-------------.           ,-------------.
- *                                         |CT/ESC|ALT/TB|           |ALT/AP|CT/ETR|
+ *                                         | CTRL |ALT/TB|           |ALT/AP| CTRL |
  *                                  ,------|------|------|           |------+------+------.
  *                                  |      |      | Home |           | PgUp |      |      |
- *                                  |  SPC |SHIFT |------|           |------| ESC  | BKSP |
+ *                                  |  SPC | BKSP |------|           |------| ESC  |ENTER |
  *                                  |      |      |  End |           | PgDn |      |      |
  *                                  `--------------------'           `--------------------'
  */
 
   [BASE] = LAYOUT_ergodox(
             // left hand
-            LSFT(KC_INSERT),    KC_1,           KC_2,          KC_3,    KC_4,    KC_5,       LGUI(KC_LEFT),
-            KC_TILD,            KC_Q,           KC_W,          KC_E,    KC_R,    KC_T,       OSL(2),
-            KC_EQUAL,           KC_A,           KC_S,          KC_D,    KC_F,    SFT_T(KC_G),
-            F(F_SFT_L),         KC_Z,           KC_X,          KC_C,    KC_V,    KC_B,       KC_TAB,
-            KC_LGUI,            LALT(KC_LCTRL), LSFT(KC_LCTRL),KC_LEFT, KC_RIGHT,
+            KC_TILD,    KC_1,           KC_2,          KC_3,    KC_4,    KC_5,       LGUI(KC_LEFT),
+            KC_TAB,     KC_Q,           KC_W,          KC_E,    KC_R,    KC_T,       OSL(2),
+            KC_EQUAL,   KC_A,           KC_S,          KC_D,    KC_F,    SFT_T(KC_G),
+            F(F_SFT_L), KC_Z,           KC_X,          KC_C,    KC_V,    KC_B,       F(F_SFT_L),
+            KC_LGUI,    LALT(KC_LCTRL), LSFT(KC_LCTRL),KC_LEFT, KC_RIGHT,
 
                                     // left thumb cluster
 
-                                       LCTL_T(KC_ESCAPE), LALT_T(KC_TAB),
-                                                          KC_HOME,
-                                    KC_SPACE, F(F_SFT_L), KC_END,
+                                               KC_LCTRL, LALT_T(KC_TAB),
+                                                         KC_HOME,
+                                    KC_SPACE, KC_BSPACE, KC_END,
 
                                                     // right hand
                                                     RGUI(KC_RIGHT), KC_6,       KC_7,   KC_8,     KC_9,             KC_0,           KC_DELETE,
                                                     OSL(1),         KC_Y,       KC_U,   KC_I,     KC_O,             KC_P,           KC_MINUS,
                                                                     SFT_T(KC_H),KC_J,   KC_K,     KC_L,             KC_SCOLON,      KC_QUOTE,
-                                                    KC_APPLICATION, KC_N,       KC_M,   KC_COMMA, KC_DOT,           KC_SLASH,       F(F_SFT_L),
+                                                    F(F_SFT_L),     KC_N,       KC_M,   KC_COMMA, KC_DOT,           KC_SLASH,       F(F_SFT_L),
                                                                                 KC_DOWN,KC_UP,    RSFT(KC_RCTRL),   RALT(KC_RCTRL), KC_LGUI,
 
                                                                     // right thumb cluster
-                                                                    RALT_T(KC_APPLICATION), RCTL_T(KC_ENTER),
+                                                                    RALT_T(KC_APPLICATION), KC_RCTRL,
                                                                     KC_PGUP,
-                                                                    KC_PGDOWN, KC_ESCAPE, KC_BSPACE
+                                                                    KC_PGDOWN, KC_ESCAPE, KC_ENTER
         ),
 
 /* Keymap 1: Symbol Layer
  *
  * ,-----------------------------------------------------.           ,-----------------------------------------------------.
- * | CTRL+INS  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |    F11    |
+ * |           |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |    F11    |
  * |-----------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
  * |           |   `  |   @  |   {  |   }  |   |  |      |           |      |   *  |   7  |   8  |   9  |   &  |    F12    |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * |           |   #  |   $  |   (  |   )  |   &  |------|           |------|   +  |   4  |   5  |   6  |   _  |           |
+ * | CTRL+INS  |   #  |   $  |   (  |   )  |   &  |------|           |------|   +  |   4  |   5  |   6  |   _  | SHIFT+INS |
  * |-----------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
  * |           |   %  |   ^  |   [  |   ]  |   "  |      |           |      |   -  |   1  |   2  |   3  |   /  |           |
  * `-----------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
@@ -108,9 +108,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [SYMB] = LAYOUT_ergodox(
             // left hand
-             LCTL(KC_INSERT),      KC_F1,          KC_F2,          KC_F3,          KC_F4,      KC_F5,      KC_TRANSPARENT,
+             KC_TRANSPARENT,       KC_F1,          KC_F2,          KC_F3,          KC_F4,      KC_F5,      KC_TRANSPARENT,
              KC_TRANSPARENT,       KC_GRAVE,       KC_AT,          KC_LCBR,        KC_RCBR,    KC_PIPE,    KC_TRANSPARENT,
-             KC_TRANSPARENT,       KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,    KC_AMPR,
+             LCTL(KC_INSERT),      KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,    KC_AMPR,
              KC_TRANSPARENT,       KC_PERC,        KC_CIRC,        KC_LBRACKET,    KC_RBRACKET,KC_DQUO,    KC_TRANSPARENT,
              KC_TRANSPARENT,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_BSLASH,
 
@@ -118,11 +118,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
 
                             // right hand
-                            KC_TRANSPARENT, KC_F6,          KC_F7,    KC_F8,      KC_F9,      KC_F10,           KC_F11,
-                            KC_TRANSPARENT, KC_ASTR,        KC_KP_7,  KC_KP_8,    KC_KP_9,    KC_AMPR,          KC_F12,
-                                            KC_PLUS,        KC_KP_4,  KC_KP_5,    KC_KP_6,    KC_UNDS,          KC_TRANSPARENT,
-                            KC_TRANSPARENT, KC_MINUS,       KC_KP_1,  KC_KP_2,    KC_KP_3,    KC_SLASH,         KC_TRANSPARENT,
-                                                            KC_EQUAL, KC_KP_0,    KC_KP_DOT,  KC_TRANSPARENT,   KC_TRANSPARENT,
+                            KC_TRANSPARENT,  KC_F6,          KC_F7,    KC_F8,      KC_F9,      KC_F10,           KC_F11,
+                            KC_TRANSPARENT,  KC_ASTR,        KC_KP_7,  KC_KP_8,    KC_KP_9,    KC_AMPR,          KC_F12,
+                                             KC_PLUS,        KC_KP_4,  KC_KP_5,    KC_KP_6,    KC_UNDS,          LSFT(KC_INSERT),
+                            KC_TRANSPARENT,  KC_MINUS,       KC_KP_1,  KC_KP_2,    KC_KP_3,    KC_SLASH,         KC_TRANSPARENT,
+                                                             KC_EQUAL, KC_KP_0,    KC_KP_DOT,  KC_TRANSPARENT,   KC_TRANSPARENT,
 
                                             // right thumb cluster
                                             KC_TRANSPARENT, KC_TRANSPARENT,
