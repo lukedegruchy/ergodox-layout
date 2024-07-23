188dcf300f548e2ba24a5766f1a7d9db9d93fcdd
Luke deGruchy <ldegruchy@gmail.com>
2024-07-23 09:47:38 -0400

    reverse gitignore for mac.

---
+++ mac/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define ONESHOT_TAP_TOGGLE 2
+
+#define FIRMWARE_VERSION u8"zb9bJ/plYDD"
